c4c52a87ce8c568664138191c5848f8a95357b23
Smmook <diegobix97@gmail.com>
2024-01-03 16:57:11 +0100

    practica3 a medias

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,49 @@
 import analisislexico.Lexico;
-import analisislexico.Token;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import analisisSintactico.TraductorPostfijo;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            String programa = Files.readString(Path.of("res/programa.txt"));
+        //Traduccion
+        String expresion = "(25 * (2 + 2)) / 2 * 3";
 
-            Lexico analizadorLexico = new Lexico(programa);
-            Token token = analizadorLexico.getNextToken();
-            while (!token.getEtiqueta().equals("end_program")) {
-                System.out.println("<" + token + ">");
-                token = analizadorLexico.getNextToken();
-            }
-            System.out.println("Fin del programa");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        TraductorPostfijo expr = new TraductorPostfijo(new Lexico(expresion));
+
+        System.out.println("La expresioÌn " + expresion
+                + " en notacioÌn postfija es " + expr.postfijo()
+                + " y su valor es " + expr.valor());
+
+        /*
+        //Analisis Lexico
+
+		Token etiquetaLexica;
+
+		String programa ="(25*(2+2))/2*3";
+
+		Lexico lexico = new Lexico(programa);
+
+
+		System.out.println("Test lexico basico \t" + programa + "\n");
+
+		do {
+
+			etiquetaLexica = lexico.getNextToken();
+
+			System.out.println(etiquetaLexica.toString());
+
+		} while (!etiquetaLexica.getEtiqueta().equals("end_program"));
+		*/
+//        try {
+//            String programa = Files.readString(Path.of("res/programa.txt"));
+//
+//            Lexico analizadorLexico = new Lexico(programa);
+//            Token token = analizadorLexico.getNextToken();
+//            while (!token.getEtiqueta().equals("end_program")) {
+//                System.out.println("<" + token + ">");
+//                token = analizadorLexico.getNextToken();
+//            }
+//            System.out.println("Fin del programa");
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
---
+++ src/analisisSintactico/TraductorPostfijo.java
@@ -0,0 +1,138 @@
+package analisisSintactico;
+
+import analisislexico.Lexico;
+import analisislexico.Token;
+
+import java.util.Stack;
+
+public class TraductorPostfijo {
+    private Token token;
+    private Lexico lexico;
+    private Stack<String> pila;
+    private String postfijo;
+
+    public TraductorPostfijo(Lexico lexico) {
+        this.lexico = lexico;
+        this.token = this.lexico.getNextToken();
+        this.pila = new Stack<String>();
+        this.postfijo = "";
+    }
+
+    public String postfijo() {
+        expresion();
+        // si se hace con print en vez de con pila, no se necesita este while
+        while (!pila.isEmpty()) {
+            postfijo += pila.remove(0) + " ";
+        }
+        return postfijo;
+    }
+
+    private void expresion() {
+        termino();
+        masTerminos();
+    }
+
+    public void factor() {
+        if (this.token.getEtiqueta().equals("open_parenthesis")) {
+            compara("open_parenthesis");
+            expresion();
+            compara("close_parenthesis");
+        } else if (this.token.getEtiqueta().equals("int")) {
+            pila.push(this.token.getValor());
+            compara("int");
+        } else {
+            System.out.println("ERROR. Se esperaba un factor");
+        }
+    }
+
+    public void termino() {
+        factor();
+        masFactores();
+    }
+
+    private void masTerminos() {
+        if (this.token.getEtiqueta().equals("add")) {
+            compara("add");
+            termino();
+            pila.push("+");
+            masTerminos();
+        } else if (this.token.getEtiqueta().equals("substract")) {
+            compara("substract");
+            termino();
+            pila.push("-");
+            masTerminos();
+        }
+    }
+
+    private void masFactores() {
+        if (this.token.getEtiqueta().equals("multiply")) {
+            compara("multiply");
+            factor();
+            pila.push("*");
+            masFactores();
+        } else if (this.token.getEtiqueta().equals("divide")) {
+            compara("divide");
+            factor();
+            pila.push("/");
+            masFactores();
+        }
+    }
+
+    //compara se utiliza cada vez que, segÃºn la gramÃ¡tica, te encuentras con una etiqueta lexica
+    //como: add, substract, open_parenthesis ...
+    public void compara(String etiquetaLexica) {
+        if (this.token.getEtiqueta().equals(etiquetaLexica)) {
+            this.token = lexico.getNextToken();
+        } else {
+            System.out.println("ERROR. Se esperaba " + etiquetaLexica);
+        }
+    }
+
+    // parq calcular el valor de esta expresiÃ³n
+    public int valor() {
+        Stack<Integer> pilaValor = new Stack<>();
+        String[] tokens = postfijo.trim().split("\\s+"); // Dividir por espacios
+
+        for (String token : tokens) {
+            if (isNumeric(token)) {
+                pilaValor.push(Integer.parseInt(token));
+            } else {
+                int num2 = pilaValor.pop(); // Segundo nÃºmero
+                int num1 = pilaValor.pop(); // Primer nÃºmero
+                switch (token) {
+                    case "+":
+                        pilaValor.push(num1 + num2);
+                        break;
+                    case "-":
+                        pilaValor.push(num1 - num2);
+                        break;
+                    case "*":
+                        pilaValor.push(num1 * num2);
+                        break;
+                    case "/":
+                        if (num2 != 0) {
+                            pilaValor.push(num1 / num2);
+                        } else {
+                            System.out.println("ERROR: DivisiÃ³n por cero.");
+                            return -1; // O cualquier otro valor de error.
+                        }
+                        break;
+                    default:
+                        System.out.println("ERROR: Operador desconocido " + token);
+                        return -1; // O cualquier otro valor de error.
+                }
+            }
+        }
+
+        return pilaValor.pop();
+    }
+
+    private boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
