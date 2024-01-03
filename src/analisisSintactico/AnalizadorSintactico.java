c8ee6249e8857b996b2905c9ece0bd7ec1e2f5b5
Smmook <diegobix97@gmail.com>
2024-01-03 20:49:21 +0100

    practica4

---
+++ src/analisisSintactico/AnalizadorSintactico.java
@@ -0,0 +1,94 @@
+package analisisSintactico;
+
+import analisislexico.Lexico;
+import analisislexico.Token;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Set;
+
+public class AnalizadorSintactico {
+    private Lexico lexico;
+    private Token componenteLexico;
+    private Hashtable<String,String> simbolos;
+    private String tipo;
+    private int tamano;
+
+    public AnalizadorSintactico(Lexico lexico) {
+        this.lexico = lexico;
+        this.componenteLexico = this.lexico.getNextToken();
+        this.simbolos = new Hashtable<String,String>();
+    }
+
+    public void analisisSintactico() {
+        declaraciones();
+    }
+
+
+    public void declaraciones() {
+        if (this.componenteLexico.getEtiqueta().equals("int") || this.componenteLexico.getEtiqueta().equals("float")) {
+            declaracion();
+            declaraciones();
+        }
+    }
+
+    public void declaracion() {
+        if (this.componenteLexico.getEtiqueta().equals("int") || this.componenteLexico.getEtiqueta().equals("float")) {
+            tipo();
+            identificadores();
+            compara("semicolon");
+        }
+    }
+
+
+    public void tipo() {
+        if (this.componenteLexico.getEtiqueta().equals("int") || this.componenteLexico.getEtiqueta().equals("float")) {
+            this.tipo = this.componenteLexico.getEtiqueta();
+            this.componenteLexico = this.lexico.getNextToken();
+        } else {
+            System.out.println("Error en el tipo: " + this.componenteLexico.toString());
+        }
+    }
+
+
+    public void identificadores() {
+        if (this.componenteLexico.getEtiqueta().equals("id")) {
+            simbolos.put(this.componenteLexico.getValor(), this.tipo);
+            this.componenteLexico = this.lexico.getNextToken();
+            masIdentificadores();
+        } else {
+            System.out.println("Error en el ident: " + this.componenteLexico.toString());
+        }
+    }
+
+    public void masIdentificadores() {
+        if (this.componenteLexico.getEtiqueta().equals("comma")) {
+            compara("comma");
+            simbolos.put(this.componenteLexico.getValor(), this.tipo);
+            this.componenteLexico = this.lexico.getNextToken();
+            masIdentificadores();
+        }
+    }
+
+
+    public void compara(String token) {
+        if(this.componenteLexico.getEtiqueta().equals(token)) {
+            this.componenteLexico = this.lexico.getNextToken();
+        }else {
+            System.out.println("Expected: " + token);
+        }
+    }
+
+    public String tablaSimbolos() {
+        String simbolos = "";
+
+        Set<Map.Entry<String, String>> s = this.simbolos.entrySet();
+        if(s.isEmpty()) System.out.println("La tabla de simbolos esta vacia\n");
+        for(Map.Entry<String, String> m : s) {
+            simbolos = simbolos + "<'" + m.getKey() + "', " +
+                    m.getValue() + "> \n";
+        }
+
+        return simbolos;
+    }
+}
---
+++ src/analisisSintactico/TestAnalizadorSintactico.java
@@ -0,0 +1,37 @@
+package analisisSintactico;
+
+import analisislexico.Lexico;
+import analisislexico.Token;
+
+public class TestAnalizadorSintactico {
+    public static void main(String[] args) {
+        boolean mostrarComponentesLexicos = false; //poner a false y no se quieren mostrar los tokens <id, a> ...
+
+        String expresion = "int a, b, c, d; float x, y, z;";
+
+        Token etiquetaLexica;
+        Lexico lexico = new Lexico(expresion);
+
+        if(mostrarComponentesLexicos) {
+
+            do {
+                etiquetaLexica = lexico.getNextToken();
+                System.out.println("<" + etiquetaLexica.toString() + ">"); //System.out.println(etiquetaLexica.toString());
+
+            }while(!etiquetaLexica.getEtiqueta().equals("end_program"));
+
+            System.out.println("");
+        }
+
+        AnalizadorSintactico compilador = new AnalizadorSintactico (new Lexico(expresion));
+
+        System.out.println("CompilaciÃ³n de sentencia de declaraciones de variables");
+        System.out.println(expresion + "\n");
+
+        compilador.analisisSintactico();
+
+        System.out.println("Tabla de sÃ­mbolos \n\n" );
+        String simbolos = compilador.tablaSimbolos();
+        System.out.println(simbolos);
+    }
+}
